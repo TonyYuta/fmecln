cbabf8a9d3fc188a0dd50e143551c33f315c80c5
TonyYuta <tonykagaya@gmail.com>
2017-06-22 22:56:45 -0700

    pom modif -- testng.xml included

--- src/test/java/com/rhmsoft/fm/HelperTest.java
+++ src/test/java/com/rhmsoft/fm/HelperTest.java
@@ -1,11 +1,6 @@
 package com.rhmsoft.fm;
 
-import io.appium.java_client.AppiumDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.net.URL;
 
 /**
  * Created by aolyva on 5/10/17.
@@ -14,13 +9,13 @@ import java.net.URL;
 public class HelperTest extends Environment {
 
     //
-    @Test(enabled = true, groups={"Helper", "DryRun", "Regression"})
+    @Test(enabled = true, groups={"helper", "dryrun", "regression", "all"})
     public void helperTest() {
         int result = helper.qqqGetDeviceSNLength("abc");
         System.out.println("result = " + result);
     }
 
-    @Test(enabled = true, groups = {"Helper", "DryRun", "Regression"})
+    @Test(enabled = true, groups = {"helper", "dryrun", "regression", "all"})
     public void deviceCapabilitiesTest() {
         String deviceCapabilitiesInfo = "";
         deviceCapabilitiesInfo = helper.deviceCapabilities("abcdf");
