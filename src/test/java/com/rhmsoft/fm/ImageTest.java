739c5e4549cf3c8ca397a0d1de39e56c60ca8f84
TonyYuta <tonykagaya@gmail.com>
2017-06-24 21:45:16 -0700

    obj added

--- src/test/java/com/rhmsoft/fm/ImageTest.java
+++ src/test/java/com/rhmsoft/fm/ImageTest.java
@@ -111,7 +111,7 @@ public class ImageTest {
 
     @Test(enabled = true, groups={"images", "dryrun", "regression", "all"})
     public void quantityImages2() {
-    	
+        homePage = new HomePage(driver);
 		homePage.qqqMethodOnHomePage();		 
 		System.out.println("================ quantityImages2() end ====================");
 
